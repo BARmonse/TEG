00667d01df604e1365ca1eb9eefcec4ea3abca25
Bruno Rius <brunorius3@gmail.com>
2025-06-01 14:44:40 -0300

    Fix in migration

--- teg-back/src/main/resources/db/migration/V1__create_users_table.sql
+++ teg-back/src/main/resources/db/migration/V1__create_users_table.sql
@@ -11,12 +11,3 @@ CREATE TABLE users (
     games_lost INTEGER DEFAULT 0,
     is_active BOOLEAN DEFAULT true
 );
-
--- Insert test users (password is 'password' BCrypt encoded)
-INSERT INTO users (username, email, password, games_played, games_won, games_lost) VALUES
-('barmonse', 'barmonse@gmail.com', '$2a$10$xn3LI/AjqicFYZFruSwve.268jZ.E.r1sYnlJtGtLsGCRXj29WE5S', 10, 5, 5),
-('Mr Guiso', 'mrguiso@gmail.com', '$2a$10$xn3LI/AjqicFYZFruSwve.268jZ.E.r1sYnlJtGtLsGCRXj29WE5S', 8, 3, 5),
-('player3', 'player3@test.com', '$2a$10$xn3LI/AjqicFYZFruSwve.268jZ.E.r1sYnlJtGtLsGCRXj29WE5S', 5, 2, 3),
-('player4', 'player4@test.com', '$2a$10$xn3LI/AjqicFYZFruSwve.268jZ.E.r1sYnlJtGtLsGCRXj29WE5S', 6, 1, 5),
-('player5', 'player5@test.com', '$2a$10$xn3LI/AjqicFYZFruSwve.268jZ.E.r1sYnlJtGtLsGCRXj29WE5S', 3, 0, 3),
-('player6', 'player6@test.com', '$2a$10$xn3LI/AjqicFYZFruSwve.268jZ.E.r1sYnlJtGtLsGCRXj29WE5S', 4, 1, 3); 
